bce197813761845972cbef0eede134039ec3b01d
TemirovaAzima <temirovaazima9@gmail.com>
2024-12-13 01:25:48 +0500

    Events end

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,55 @@
 import React from 'react'
-import './index.css'
-import {PiAlienThin, PiAndroidLogoDuotone} from 'react-icons/pi'
-import {DiAndroid} from "react-icons/di";
+
 const App = () => {
-    const dizayn = {color:'red', textAlign:'center'}
     return (
-        <>
-            <h1 style={dizayn}>Hello World
-                <PiAlienThin/>
-                <PiAndroidLogoDuotone />
-                <DiAndroid />
-            </h1>
-        </>
+        <section>
+            {/*<Button/>*/}
+            {/*<Copy/>*/}
+            <Move/>
+        </section>
     )
 }
 export default App
+
+// const Button =()=>{
+//     // function showAlert(){
+//     //     alert("Hello event ishladi")
+//     // }
+//     function clickHadler(){
+//         alert(Math.round(Math.random()*10))
+//     }
+//     return (
+//         <div>
+//             {/*<button onClick={()=>alert('event ishladi')}>Click</button>*/}
+//             {/*<button onClick={<h1>fhjf</h1>}>Click</button>*/}
+//             <button onClick={clickHadler}>Click</button>
+//         </div>
+//     )
+// }
+// const Copy = ()=>{
+//     const showAlert = ()=> alert('Copied')
+//
+//     return (
+//         <p onCopy={()=> alert("Copied")}>
+//             clear, transparent, translucent, limpid mean
+//             capable of being seen through. clear implies absence of
+//             cloudiness, haziness, or muddiness.clear, transparent, translucent, limpid mean capable of being seen through.
+//             clear implies absence of cloudiness, haziness, or muddiness.
+//         </p>
+//     )
+// }
+const Move = ()=>{
+    function mouseMove(){
+        alert("MouseMove event worked")
+        console.log('MouseMove event worked')
+    }
+    return (
+        <p onMouseMove={mouseMove}>
+            clear, transparent, translucent, limpid mean
+            capable of being seen through. clear implies absence of
+            cloudiness, haziness, or muddiness.clear, transparent, translucent,
+            limpid mean capable of being seen through.
+            clear implies absence of cloudiness, haziness, or muddiness.
+        </p>
+    )
+}
